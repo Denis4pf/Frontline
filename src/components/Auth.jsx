abc00ee52f5c989221840f72074cbcd701edc080
Denis4pf <denis.onwonga@student.moringaschool.com>
2023-04-05 23:02:48 +0300

    hot fix

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
-import Login from './Login'
-import Signup from './Signup'
+// import React, { useState } from 'react'
 // import Login from './Login'
-// import SignupForm from './Signup'
+// import Signup from './Signup'
+// // import Login from './Login'
+// // import SignupForm from './Signup'
 
-export default function Auth({onLogin}) {
+// export default function Auth({onLogin}) {
 
-    const [login, setLogin] = useState(true)
+//     const [login, setLogin] = useState(true)
 
-    function handleClick(e) {
-        e.preventDefault()
-        setLogin(!login)
-    }
+//     function handleClick(e) {
+//         e.preventDefault()
+//         setLogin(!login)
+//     }
 
-    return (
-        <div>
-            {login ? <Login handleClick={handleClick}  onLogin={onLogin}/> : <Signup handleClick={handleClick} onLogin={onLogin}/>}
-        </div>
-    )
-}
+//     return (
+//         <div>
+//             {login ? <Login handleClick={handleClick}  onLogin={onLogin}/> : <Signup handleClick={handleClick} onLogin={onLogin}/>}
+//         </div>
+//     )
+// }
